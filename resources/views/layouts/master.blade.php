1ca7b05cb960f14881e6d117ef9f69b482c297c0
TomohiroTsubaki <ttsubaki928@gmail.com>
2023-06-12 14:45:42 +0700

    create sidebar partial

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,18 +12,7 @@
 
 <body>
   <div class="container">
-    <div class="sidebar">
-      <div class="sidebar-container">
-        <a class="sidebar-link" href="{{ route('home') }}"> <!-- Diperbarui -->
-          <span class="material-icons sidebar-icon">home</span>
-          <p class="sidebar-text">Home</p>
-        </a>
-        <a class="sidebar-link" href="{{ route('tasks.index') }}"> <!-- Diperbarui -->
-          <span class="material-icons sidebar-icon">list</span>
-          <p class="sidebar-text">Task List</p>
-        </a>
-      </div>
-    </div>
+    @include('partials.sidebar')
     <div class="main">
       @yield('main')
     </div>
